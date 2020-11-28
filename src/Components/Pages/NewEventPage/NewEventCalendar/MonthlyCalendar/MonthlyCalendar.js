97542df3067b5faaa62e448219d5ddce3ab1ea16
Kalin Ramsey <kalinramsey@gmail.com>
2020-11-28 10:53:33 -0700

    made event creation form, and started on calendar creation form

---
+++ src/Components/Pages/NewEventPage/NewEventCalendar/MonthlyCalendar/MonthlyCalendar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './MonthlyCalendar.css';
+
+export class MonthlyCalendar extends Component {
+  render() {
+    return (
+      <div>
+        Monthly Calendar
+      </div>
+    )
+  }
+}
+
+export default MonthlyCalendar
