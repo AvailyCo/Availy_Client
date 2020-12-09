c7444e8486edea7a9d786dec8b1f7285a3d99975
Sunny Lee <leesunny507@gmail.com>
2020-12-09 03:07:04 -0500

    finished landing steps but svg import issue

--- src/TEXT.js
+++ src/TEXT.js
@@ -1,76 +1,49 @@
 /* eslint-disable import/no-anonymous-default-export */
-/* import landing0 from './Assets/Landing0.png';
-import landing1 from './Assets/Landing1.png';
-//import landing2 from './Assets/Landing2.png';
-
-import Landing2 from './Assets/landing2.svg';
-
-//import landing3 from './Assets/Landing3.png';
-import landing4 from './Assets/Landing4.png';
-import landing5 from './Assets/Landing5.png'; */
-
-
-/* import { ReactComponent as Landing1 } from './Assets/Landing1.svg'; */
+import { ReactComponent as Landing1 } from './Assets/Landing1.svg';
 import { ReactComponent as Landing2 } from './Assets/Landing2.svg';
-/* import { ReactComponent as Landing3 } from './Assets/Landing3.svg';
-import { ReactComponent as Landing4 } from './Assets/Landing4.svg'; */
+import { ReactComponent as Landing3 } from './Assets/Landing3.svg';
+import { ReactComponent as Landing4 } from './Assets/Landing4.svg';
 
+import { ReactComponent as Test1 } from './Assets/text.svg';
 
 import user1 from './Assets/User1.jpg';
 import user2 from './Assets/User2.jpg';
 import user3 from './Assets/User3.jpeg';
-
-
-
-
 
 export const HeroText = {
   "heading": "Find a time that works for all",
   "details": "Having trouble coordinating events with your friends? Availy can help! We sort through the schedules of multiple users to present you with the best times and dates to get your people together."
 }
 
-
-
-
-
-
-
-
-
 export const LandingSectionText = [
   {
     "id": 1,
     "header": "Step 1: Build your Agenda",
     "details": "By filling out your general availability, your friends and groups are better equipped to create events that work around your schedule without causing any conflict!",
-    /* "svgLocation": <Landing1 />, */
+    "svgLocation": <Landing1 />,
   },
   {
     "id": 2,
     "header": "Step 2: Invite your friends",
     "details": "If your friends haven't signed up yet, don't worry - everyone is invited! With Availy, you can send invites via email, collect user's scheduling data, and continue to plan get-togethers with the utmost efficiency. You can also group your contacts together and invite everyone all at once!",
     "svgLocation": <Landing2 />,
+    /* "svgLocation": <Test1 /> */
   },
   {
     "id": 3,
     "header": "Step 3: Find times that work",
     "details": "Once everyone has provided their most convenient times to meet, Availy's algorithm will filter through everyone's availability and present you with the most convenient times and days to get all parties together.",
-    /* "svgLocation": <Landing3 />, */
+    "svgLocation": <Landing3 />,
+    /* "svgLocation": <Test1 /> */
   },
   {
     "id": 4,
     "header": "Step 4: Create an event",
     "details": "When you plan ahead with Availy, your events' success multiplies exponentially. By conveniently scheduling around others' schedules, you are much more likely to get a higher turn out!",
-    /*  "svgLocation": <Landing4 />, */
+    "svgLocation": <Landing4 />,
+    /* "svgLocation": <Test1 /> */
   }
 ]
-
-
-
-
-
-
-
-
 
 export const SuccessStoriesText = [
   {
